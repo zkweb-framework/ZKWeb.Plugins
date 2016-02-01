d2ed45585966335f8d07ef1e74b616e2701342db
303248153 <303248153@qq.com>
2016-02-01 11:55:43 +0800

    改进菜单导航栏的显示效果

---
+++ Common.MenuPageBase/static/common.menu_page_base.js/menu_groups_nav.js
@@ -0,0 +1,8 @@
+﻿$(function() {
+	var path = location.pathname + location.search;
+	var $link = $(".menu_groups_nav").find("a").filter(function () {
+		return $(this).attr("href") == path;
+	});
+	$link.closest("li").addClass("active");
+	$link.closest(".collapse").addClass("in");
+});
