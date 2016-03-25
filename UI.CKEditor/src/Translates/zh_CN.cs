c9699a8767bb829071f22eebaebe11f10e697ba8
303248153 <303248153@qq.com>
2016-03-25 16:11:37 +0800

    csproj

---
+++ UI.CKEditor/src/Translates/zh_CN.cs
@@ -0,0 +1,31 @@
+﻿using DryIocAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZKWeb.Localize.Interfaces;
+using ZKWeb.Utils.Extensions;
+
+namespace ZKWeb.UI.CKEditor.Article.src.Translates {
+	/// <summary>
+	/// 中文翻译
+	/// </summary>
+	[ExportMany, SingletonReuse]
+	public class zh_CN : ITranslateProvider {
+		private static HashSet<string> Codes = new HashSet<string>() { "zh-CN" };
+		private static Dictionary<string, string> Translates = new Dictionary<string, string>()
+		{
+			{ "CKEditor", "CKEditor" },
+			{ "Provide ckeditor form field", "提供CKEditor表单字段" }
+		};
+
+		public bool CanTranslate(string code) {
+			return Codes.Contains(code);
+		}
+
+		public string Translate(string text) {
+			return Translates.GetOrDefault(text);
+		}
+	}
+}
