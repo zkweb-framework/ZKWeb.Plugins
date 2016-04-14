d37e69abf665ea3a5e3049608b8e06782b8fdd35
303248153 <303248153@qq.com>
2016-04-14 14:00:22 +0800

    改进验证码强度

--- Common.Base/src/Managers/CaptchaManager.cs
+++ Common.Base/src/Managers/CaptchaManager.cs
@@ -22,6 +22,30 @@ namespace ZKWeb.Plugins.Common.Base.src.Managers {
 		/// </summary>
 		public const int DefaultDigits = 4;
 		/// <summary>
+		/// 每个字符的宽度
+		/// </summary>
+		public const int ImageWidthPerChar = 20;
+		/// <summary>
+		/// 验证码图片的高度
+		/// </summary>
+		public const int ImageHeight = 32;
+		/// <summary>
+		/// 图片边距
+		/// </summary>
+		public const int ImagePadding = 5;
+		/// <summary>
+		/// 干扰线数量
+		/// </summary>
+		public const int InterferenceLines = 3;
+		/// <summary>
+		/// 字符图表的最大拉伸量
+		/// </summary>
+		public const int CharGraphicMaxPadding = 5;
+		/// <summary>
+		/// 字符图表的最大旋转量
+		/// </summary>
+		public const int CharGraphicMaxRotate = 35;
+		/// <summary>
 		/// 生成验证码后最小保留会话的时间
 		/// </summary>
 		public const int MakeSessionAliveAtLeast = 30;
@@ -33,40 +57,54 @@ namespace ZKWeb.Plugins.Common.Base.src.Managers {
 		/// <summary>
 		/// 生成验证码并储存验证码到会话中
 		/// </summary>
+		/// <param name="key">使用的键名</param>
+		/// <param name="digits">验证码位数</param>
+		/// <param name="chars">使用的字符列表</param>
+		/// <returns></returns>
 		public virtual Image Generate(string key, int digits = DefaultDigits, string chars = null) {
 			// 生成验证码
-			var captchaCode = RandomUtils.RandomString(digits, "23456789ABCDEFGHJKLMNPQRSTUWXYZ");
-			var image = new Bitmap(digits * 20, 32);
-			var imageRect = new Rectangle(0, 0, image.Width, image.Height);
-			var font = new Font("Arial", 20, FontStyle.Regular);
-			var brush = new SolidBrush(Color.Black);
+			var captchaCode = RandomUtils.RandomString(
+				digits, chars ?? "23456789ABCDEFGHJKLMNPQRSTUWXYZ");
+			var image = new Bitmap(ImageWidthPerChar * digits + ImagePadding * 2, ImageHeight);
+			var font = new Font("Arial", ImageWidthPerChar, FontStyle.Bold);
+			var rand = RandomUtils.Generator;
 			using (var graphic = Graphics.FromImage(image)) {
 				// 描画背景
-				HatchStyle backgroundStyle = (HatchStyle)RandomUtils.Generator.Next(18, 52);
-				while (!Enum.IsDefined(typeof(HatchStyle), backgroundStyle)) {
-					backgroundStyle = (HatchStyle)RandomUtils.Generator.Next(18, 52);
+				var backgroundBrush = new SolidBrush(Color.White);
+				graphic.FillRectangle(backgroundBrush, new RectangleF(0, 0, image.Width, image.Height));
+				// 添加干扰线
+				var pen = new Pen(Color.Black);
+				for (int x = 0; x < InterferenceLines; ++x) {
+					var pointStart = new Point(rand.Next(image.Width), rand.Next(image.Height));
+					var pointFinish = new Point(rand.Next(image.Width), rand.Next(image.Height));
+					graphic.DrawLine(pen, pointStart, pointFinish);
 				}
-				graphic.FillRectangle(
-					new HatchBrush(backgroundStyle, Color.Gray, Color.White), imageRect);
-				// 描画文本，然后不规则拉伸
-				GraphicsPath path = new GraphicsPath();
-				path.AddString(captchaCode, font.FontFamily, (int)font.Style, font.Size, imageRect,
-					new StringFormat()
-					{
-						Alignment = StringAlignment.Center,
-						LineAlignment = StringAlignment.Center
-					});
-				int padding = 5;
-				Func<int> randomPadding = () => RandomUtils.Generator.Next(padding);
-				var points = new PointF[] {
-						new PointF(randomPadding(), randomPadding()),
-						new PointF(image.Width - randomPadding(), randomPadding()),
-						new PointF(randomPadding(), image.Height - randomPadding()),
-						new PointF(image.Width - randomPadding(), image.Height - randomPadding()),
+				// 逐个字符描画，并进行不规则拉伸
+				var stringFormat = StringFormat.GenericDefault;
+				var randomPadding = new Func<int>(() => rand.Next(CharGraphicMaxPadding));
+				var textBrush = new SolidBrush(Color.Black);
+				for (int x = 0; x < captchaCode.Length; ++x) {
+					var path = new GraphicsPath();
+					var rect = new RectangleF(
+						ImageWidthPerChar * x + ImagePadding, ImagePadding,
+						ImageWidthPerChar, image.Height - ImagePadding);
+					var str = captchaCode[x].ToString();
+					path.AddString(str, font.FontFamily, (int)font.Style, font.Size, rect, stringFormat);
+					// 拉伸
+					var points = new PointF[] {
+						new PointF(rect.X + randomPadding(), randomPadding()),
+						new PointF(rect.X + rect.Width - randomPadding(), randomPadding()),
+						new PointF(rect.X + randomPadding(), image.Height - randomPadding()),
+						new PointF(rect.X + rect.Width - randomPadding(), image.Height - randomPadding()),
 					};
-				path.Warp(points, imageRect);
-				graphic.FillPath(
-					new HatchBrush(HatchStyle.LargeConfetti, Color.Black), path);
+					path.Warp(points, rect);
+					// 旋转
+					var matrix = new Matrix();
+					matrix.RotateAt(rand.Next(-CharGraphicMaxRotate, CharGraphicMaxRotate), rect.Location);
+					graphic.Transform = matrix;
+					// 描画到图层
+					graphic.FillPath(textBrush, path);
+				}
 			}
 			// 储存到会话，会话的过期时间最少是30分钟
 			var sessionManager = Application.Ioc.Resolve<SessionManager>();
@@ -80,8 +118,10 @@ namespace ZKWeb.Plugins.Common.Base.src.Managers {
 
 		/// <summary>
 		/// 获取当前验证码，但不删除
-		/// 这个函数一般只用于单元测试，检查请使用Check函数
+		/// 这个函数一般只用于单元测试或语音提示，检查请使用Check函数
 		/// </summary>
+		/// <param name="key">使用的键名</param>
+		/// <returns></returns>
 		public virtual string GetWithoutRemove(string key) {
 			var sessionManager = Application.Ioc.Resolve<SessionManager>();
 			var session = sessionManager.GetSession();
@@ -91,6 +131,9 @@ namespace ZKWeb.Plugins.Common.Base.src.Managers {
 		/// <summary>
 		/// 检查验证码是否正确，检查后删除原验证码
 		/// </summary>
+		/// <param name="key">使用的键名</param>
+		/// <param name="actualCode">收到的验证码</param>
+		/// <returns></returns>
 		public virtual bool Check(string key, string actualCode) {
 			var sessionManager = Application.Ioc.Resolve<SessionManager>();
 			var session = sessionManager.GetSession();
