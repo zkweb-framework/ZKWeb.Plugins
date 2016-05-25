8365c506a8172ef5a895b638577bc8f632b9ba6d
303248153 <303248153@qq.com>
2016-05-25 15:00:13 +0800

    支持在商品详情页显示卖家

---
+++ Shopping.Product/src/ProductSalesInfoDisplayFields/SellerDisplayField.cs
@@ -0,0 +1,31 @@
+﻿using DryIocAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZKWeb.Plugins.Shopping.Product.src.Model;
+using ZKWeb.Database;
+using ZKWeb.Plugins.Shopping.Product.src.Database;
+using System.Web;
+
+namespace ZKWeb.Plugins.Shopping.Product.src.ProductSalesInfoDisplayFields {
+	/// <summary>
+	/// 卖家
+	/// </summary>
+	[ExportMany]
+	public class SellerDisplayField : IProductSalesInfoDisplayField {
+		/// <summary>
+		/// 名称
+		/// </summary>
+		public string Name { get { return "Seller"; } }
+
+		/// <summary>
+		/// 获取显示的Html
+		/// </summary>
+		public string GetDisplayHtml(DatabaseContext context, Database.Product product) {
+			var seller = product.Seller;
+			return seller == null ? null : HttpUtility.HtmlEncode(seller.Username);
+		}
+	}
+}
