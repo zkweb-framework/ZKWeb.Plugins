bfda0126ca959228fd35cc90ff9726796535c7d9
303248153 <303248153@qq.com>
2016-05-27 14:43:25 +0800

    修改demo首页内容，修改移动端菜单栏样式

--- Demo/src/Plugin.cs
+++ Demo/src/Plugin.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ZKWeb.Plugin.Interfaces;
 using ZKWeb.Plugins.Common.Captcha.src.Managers;
+using ZKWeb.Templating.AreaSupport;
 
 namespace ZKWeb.Plugins.Demo.src {
 	/// <summary>
@@ -21,6 +22,10 @@ namespace ZKWeb.Plugins.Demo.src {
 			// 当前DEMO环境不支持TTS语音，手动设置不支持
 			var captchaManager = Application.Ioc.Resolve<CaptchaManager>();
 			captchaManager.SupportCaptchaAudio = false;
+			// 注册默认模块
+			var areaManager = Application.Ioc.Resolve<TemplateAreaManager>();
+			areaManager.GetArea("header_menubar").DefaultWidgets.Add("demo.widgets/demo_nav_menu");
+			areaManager.GetArea("index_top_area_1").DefaultWidgets.Add("demo.widgets/demo_index");
 		}
 	}
 }
