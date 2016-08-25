5e2f26298b67ec839a7fbebdbdbf3ab02e1e1936
dudes-come <dudes@qq.com>
2016-08-25 16:45:29 +0800

    更新common.base的代码

---
+++ src/ZKWeb.Plugins/Common.Base/src/Domain/Services/SessionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ZKWeb.Plugins.Common.Base.src.Domain.Entities;
+using ZKWebStandard.Ioc;
+
+namespace ZKWeb.Plugins.Common.Base.src.Domain.Services {
+	/// <summary>
+	/// 会话管理器
+	/// </summary>
+	[ExportMany, SingletonReuse]
+	public class SessionManager : DomainServiceBase<Session, Guid> {
+		/// <summary>
+		/// 删除已过期的会话
+		/// </summary>
+		public virtual long RemoveExpiredSessions() {
+			var now = DateTime.UtcNow;
+			using (UnitOfWork.Scope()) {
+				return Repository.BatchDelete(s => s.Expires < now);
+			}
+		}
+	}
+}
