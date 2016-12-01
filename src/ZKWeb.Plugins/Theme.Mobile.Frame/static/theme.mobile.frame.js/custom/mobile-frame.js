f59cd0f10726a4e058c2690c1e45148e197d5231
dudes-come <dudes@qq.com>
2016-12-01 16:17:14 +0800

    更新手机版样式

--- src/ZKWeb.Plugins/Theme.Mobile.Frame/static/theme.mobile.frame.js/custom/mobile-frame.js
+++ src/ZKWeb.Plugins/Theme.Mobile.Frame/static/theme.mobile.frame.js/custom/mobile-frame.js
@@ -5,4 +5,17 @@
 $(function () {
 	// 自动设置标题栏
 	$(".top-navbar .title").text($("title").text());
+
+	// 侧边栏展开和收缩
+	var bottomPadding = 20;
+	$(document).on("click", ".top-navbar .sidebar-menu", function () {
+		var $sidebar = $(".top-sidebar");
+		$("html, body").animate({ scrollTop: 0 }, 100);
+		if (!$sidebar.hasClass("control-sidebar-open")) {
+			$sidebar.addClass("control-sidebar-open");
+			$sidebar.css("min-height", $(".page-body").outerHeight() + bottomPadding + "px");
+		} else {
+			$sidebar.removeClass("control-sidebar-open");
+		}
+	});
 });
