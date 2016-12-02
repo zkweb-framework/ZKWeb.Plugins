4b7ce907bff0e8a4675ae0fc61824fef6aa947ae
dudes-come <dudes@qq.com>
2016-12-02 15:07:19 +0800

    改进手机版菜单页

--- src/ZKWeb.Plugins/Theme.Mobile.Frame/static/theme.mobile.frame.js/components.js
+++ src/ZKWeb.Plugins/Theme.Mobile.Frame/static/theme.mobile.frame.js/components.js
@@ -1,2 +1,3 @@
 ﻿// @koala-append "custom/mobile-frame.js"
+// @koala-append "custom/mobile-menupage.js"
 // @koala-append "custom/pace-hidden.js"
---
+++ src/ZKWeb.Plugins/Theme.Mobile.Frame/static/theme.mobile.frame.js/custom/mobile-menupage.js
@@ -0,0 +1,11 @@
+﻿/*
+	手机版页面的功能 (菜单页)
+*/
+
+$(function () {
+	// 如果当前的url的menu-opened参数等于true则只显示菜单
+	var uri = new Uri(location.href);
+	if (uri.getQueryParamValue("menu-opened") == "true") {
+		$("body").addClass("menu-page-menu-opened");
+	}
+});
