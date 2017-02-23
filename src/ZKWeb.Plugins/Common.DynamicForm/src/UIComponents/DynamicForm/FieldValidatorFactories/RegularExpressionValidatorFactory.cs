efa6ff518e9d85d96957de1e31fbce7c7d520f5a
dudes-come <dudes@qq.com>
2017-02-23 12:10:11 +0800

    更新动态表单

--- src/ZKWeb.Plugins/Common.DynamicForm/src/UIComponents/DynamicForm/FieldValidatorFactories/RegularExpressionValidatorFactory.cs
+++ src/ZKWeb.Plugins/Common.DynamicForm/src/UIComponents/DynamicForm/FieldValidatorFactories/RegularExpressionValidatorFactory.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using ZKWeb.Plugins.Common.DynamicForm.src.UIComponents.DynamicForm.Interfaces;
+using ZKWebStandard.Extensions;
+using ZKWebStandard.Ioc;
 
-namespace ZKWeb.Plugins.Common.DynamicForm.src.UIComponents.DynamicForm.FieldValidatorFactories
-{
-    public class RegularExpressionValidatorFactory
-    {
-    }
+namespace ZKWeb.Plugins.Common.DynamicForm.src.UIComponents.DynamicForm.FieldValidatorFactories {
+	/// <summary>
+	/// 生成表达式验证属性
+	/// </summary>
+	[ExportMany(ContractKey = "RegularExpression")]
+	public class RegularExpressionValidatorFactory : IDynamicFormFieldValidatorFactory {
+		/// <summary>
+		/// 生成表达式验证属性
+		/// </summary>
+		public Attribute Create(IDictionary<string, object> validatorData) {
+			var pattern = validatorData.GetOrDefault<string>("Pattern");
+			return new RegularExpressionAttribute(pattern);
+		}
+	}
 }
--- src/ZKWeb.Plugins/Common.DynamicForm/src/UIComponents/DynamicForm/FieldValidatorFactories/StringLengthValidatorFactory.cs
+++ src/ZKWeb.Plugins/Common.DynamicForm/src/UIComponents/DynamicForm/FieldValidatorFactories/StringLengthValidatorFactory.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using ZKWeb.Plugins.Common.DynamicForm.src.UIComponents.DynamicForm.Interfaces;
+using ZKWebStandard.Extensions;
+using ZKWebStandard.Ioc;
 
-namespace ZKWeb.Plugins.Common.DynamicForm.src.UIComponents.DynamicForm.FieldValidatorFactories
-{
-    public class StringLengthValidatorFactory
-    {
-    }
+namespace ZKWeb.Plugins.Common.DynamicForm.src.UIComponents.DynamicForm.FieldValidatorFactories {
+	/// <summary>
+	/// 生成长度验证属性
+	/// </summary>
+	[ExportMany(ContractKey = "StringLength")]
+	public class StringLengthValidatorFactory : IDynamicFormFieldValidatorFactory {
+		/// <summary>
+		/// 生成长度验证属性
+		/// </summary>
+		public Attribute Create(IDictionary<string, object> validatorData) {
+			var maximumLength = validatorData.GetOrDefault<int>("MaximumLength");
+			var minimumLength = validatorData.GetOrDefault<int>("MinimumLength");
+			return new StringLengthAttribute(maximumLength) { MinimumLength = minimumLength };
+		}
+	}
 }
