fac6c39166d61534b2b8a62f52ee24dca0ad97c9
dudes-come <dudes@qq.com>
2017-03-02 17:21:35 +0800

    添加Finance.Payment.AlipayMobile插件

---
+++ src/ZKWeb.Plugins/Finance.Payment.AlipayMobile/src/Components/Translates/zh_CN.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ZKWeb.Localize;
+using ZKWebStandard.Extensions;
+using ZKWebStandard.Ioc;
+
+namespace ZKWeb.Plugins.Finance.Payment.AlipayMobile.src.Components.Translates {
+	/// <summary>
+	/// 中文翻译
+	/// </summary>
+	[ExportMany, SingletonReuse]
+	public class zh_CN : ITranslateProvider {
+		private static HashSet<string> Codes = new HashSet<string>() { "zh-CN" };
+		private static Dictionary<string, string> Translates = new Dictionary<string, string>()
+		{
+			// TODO: 翻译到其他语言
+			{ "AlipayMobilePaymentApi", "支付宝手机支付" },
+			{ "Support pay transactions by alipay mobile", "支持在移动端通过支付宝支付交易" },
+		};
+
+		public bool CanTranslate(string code) {
+			return Codes.Contains(code);
+		}
+
+		public string Translate(string text) {
+			return Translates.GetOrDefault(text);
+		}
+	}
+}
