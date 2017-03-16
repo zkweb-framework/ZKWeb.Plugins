e111f34917f6682b8da581e144c88c50cafb13f4
dudes-come <dudes@qq.com>
2017-03-16 10:35:49 +0800

    更新主题管理功能

--- src/ZKWeb.Plugins/Theme.VisualEditor/src/Domain/Services/VisualThemeManager.cs
+++ src/ZKWeb.Plugins/Theme.VisualEditor/src/Domain/Services/VisualThemeManager.cs
@@ -1,6 +1,13 @@
-﻿using ZKWeb.Plugins.Common.Base.src.Domain.Services.Bases;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using ZKWeb.Plugins.Common.Base.src.Domain.Services.Bases;
 using ZKWeb.Plugins.Theme.VisualEditor.src.Domain.Structs;
+using ZKWeb.Storage;
 using ZKWeb.Templating.DynamicContents;
+using ZKWebStandard.Extensions;
 using ZKWebStandard.Ioc;
 
 namespace ZKWeb.Plugins.Theme.VisualEditor.src.Domain.Services {
@@ -13,24 +20,100 @@ namespace ZKWeb.Plugins.Theme.VisualEditor.src.Domain.Services {
 		/// 最多保留100份备份
 		/// </summary>
 		public static int KeepBackups = 100;
+		/// <summary>
+		/// 主题文件夹的名称
+		/// </summary>
+		public static string ThemeDirectoryName = "themes";
+		/// <summary>
+		/// 备份主题文件夹的名称
+		/// </summary>
+		public static string BackupThemeDirectoryName = "theme_backups";
+
+		/// <summary>
+		/// 导出当前主题到数据流
+		/// 包括以下文件
+		///		theme.json 主题信息
+		///		App_Data\areas下的文件
+		///	不包括以下文件
+		///		App_Data\static下的文件
+		///		App_Data\templates下的文件
+		///		这些文件可以手动添加到包里面
+		/// </summary>
+		/// <param name="info">主题信息</param>
+		/// <param name="stream">数据流</param>
+		protected virtual void ExportThemeToStream(VisualThemeInfo info, Stream stream) {
+			var fileStorage = Application.Ioc.Resolve<IFileStorage>();
+			var areasDir = fileStorage.GetStorageDirectory("areas");
+			using (var archive = new ZipArchive(stream, ZipArchiveMode.Create)) {
+				// 添加主题信息
+				var infoEntry = archive.CreateEntry("theme.json");
+				using (var writer = new StreamWriter(infoEntry.Open())) {
+					writer.Write(JsonConvert.SerializeObject(info));
+				}
+				// 添加areas下的文件
+				foreach (var widgetsFile in areasDir.EnumerateFiles()) {
+					var widgetsEntry = archive.CreateEntry("areas/" + widgetsFile.Filename);
+					using (var writer = new StreamWriter(widgetsEntry.Open())) {
+						writer.Write(widgetsFile.ReadAllText());
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// 从数据流导入主题覆盖当前的主题
+		/// </summary>
+		/// <param name="stream">数据流</param>
+		protected virtual void ImportThemeFromStream(Stream stream) {
+			var fileStorage = Application.Ioc.Resolve<IFileStorage>();
+			// 删除areas文件夹
+			fileStorage.GetStorageDirectory("areas").Delete();
+			// 解压缩主题中的所有文件
+			// OpenWrite的时候会自动创建文件夹
+			using (var archive = new ZipArchive(stream, ZipArchiveMode.Read)) {
+				foreach (var entry in archive.Entries) {
+					var storageFile = fileStorage.GetStorageFile(entry.FullName);
+					using (var writeStream = storageFile.OpenWrite())
+					using (var readStream = entry.Open()) {
+						readStream.CopyTo(writeStream);
+					}
+				}
+			}
+		}
 
 		/// <summary>
 		/// 备份当前主题
 		/// </summary>
 		public virtual void BackupUsingTheme() {
-			// TODO: 清理旧的备份文件
-			// TODO: 生成备份文件
+			// 清理旧的备份文件
+			var fileStorage = Application.Ioc.Resolve<IFileStorage>();
+			var backupThemeDir = fileStorage.GetStorageDirectory(BackupThemeDirectoryName);
+			var files = backupThemeDir.EnumerateFiles().OrderByDescending(f => f.Filename).ToList();
+			foreach (var file in files.Skip(KeepBackups)) {
+				file.Delete();
+			}
+			// 生成主题信息
+			var info = new VisualThemeInfo();
+			info.Name = "AutoBackup";
+			info.Author = "System";
+			info.Version = "1.0";
+			// 生成备份文件
+			var backupFileName = DateTime.UtcNow.ToClientTime().ToString("yyyyMMdd_HHmmss") + ".zip";
+			var backupFile = fileStorage.GetStorageFile(BackupThemeDirectoryName, backupFileName);
+			using (var stream = backupFile.OpenWrite()) {
+				ExportThemeToStream(info, stream);
+			}
 		}
 
 		/// <summary>
-		/// 导出主题
+		/// 导出当前主题
 		/// </summary>
 		public virtual void ExportTheme() {
 
 		}
 
 		/// <summary>
-		/// 导入主题
+		/// 导入主题覆盖当前的主题
 		/// </summary>
 		public virtual void ImportTheme() {
 
---
+++ src/ZKWeb.Plugins/Theme.VisualEditor/src/Domain/Structs/VisualThemeInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ZKWeb.Plugins.Theme.VisualEditor.src.Domain.Structs {
+	/// <summary>
+	/// 可视化主题信息
+	/// </summary>
+	public class VisualThemeInfo {
+		/// <summary>
+		/// 主题名称
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// 主题描述
+		/// </summary>
+		public string Description { get; set; }
+		/// <summary>
+		/// 主题作者
+		/// </summary>
+		public string Author { get; set; }
+		/// <summary>
+		/// 主题版本
+		/// </summary>
+		public string Version { get; set; }
+		/// <summary>
+		/// 预览图的Base64
+		/// </summary>
+		public string PreviewImageBase64 { get; set; }
+
+		/// <summary>
+		/// 设置预览图
+		/// </summary>
+		/// <param name="image">预览图</param>
+		void SetPreviewImage(Image image) {
+			using (var memoryStream = new MemoryStream()) {
+				image.Save(memoryStream, ImageFormat.Png);
+				memoryStream.Seek(0, SeekOrigin.Begin);
+				PreviewImageBase64 = Convert.ToBase64String(memoryStream.ToArray());
+			}
+		}
+	}
+}
