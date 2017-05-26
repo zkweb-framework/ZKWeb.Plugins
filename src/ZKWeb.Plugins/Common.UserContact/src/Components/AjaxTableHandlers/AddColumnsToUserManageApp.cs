1dd40135c6bc62f2b1bcddd80a33ab0ca2e9a077
dudes-come <dudes@qq.com>
2017-05-26 08:43:36 +0800

    允许按联系人查询用户

--- src/ZKWeb.Plugins/Common.UserContact/src/Components/AjaxTableHandlers/AddColumnsToUserManageApp.cs
+++ src/ZKWeb.Plugins/Common.UserContact/src/Components/AjaxTableHandlers/AddColumnsToUserManageApp.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using ZKWeb.Plugins.Common.Admin.src.Controllers;
 using ZKWeb.Plugins.Common.Admin.src.Domain.Entities;
+using ZKWeb.Plugins.Common.Base.src.Domain.Repositories.Interfaces;
 using ZKWeb.Plugins.Common.Base.src.UIComponents.AjaxTable;
 using ZKWeb.Plugins.Common.Base.src.UIComponents.AjaxTable.Extensions;
 using ZKWeb.Plugins.Common.Base.src.UIComponents.AjaxTable.Interfaces;
 using ZKWeb.Plugins.Common.Base.src.UIComponents.BaseTable;
+using ZKWeb.Plugins.Common.Base.src.UIComponents.Forms;
+using ZKWeb.Plugins.Common.Base.src.UIComponents.Forms.Attributes;
 using ZKWeb.Plugins.Common.UserContact.src.Domain.Services;
 using ZKWebStandard.Extensions;
 using ZKWebStandard.Ioc;
@@ -20,12 +23,28 @@ namespace ZKWeb.Plugins.Common.UserContact.src.Components.AjaxTableHandlers {
 	[ExportMany]
 	public class AddColumnsToUserManageApp :
 		IAjaxTableExtraHandler<User, Guid, UserCrudController.TableHandler> {
-		public void BuildTable(AjaxTableBuilder table, AjaxTableSearchBarBuilder searchBar) { }
+		public void BuildTable(AjaxTableBuilder table, AjaxTableSearchBarBuilder searchBar) {
+			searchBar.Conditions.Add(new FormField(new TextBoxFieldAttribute("Contact Information")));
+		}
 
 		public Func<TResult> WrapQueryMethod<TResult>(
 			AjaxTableSearchRequest request, Func<TResult> queryMethod) { return queryMethod; }
 
-		public void OnQuery(AjaxTableSearchRequest request, ref IQueryable<User> query) { }
+		public void OnQuery(AjaxTableSearchRequest request, ref IQueryable<User> query) {
+			var contact = request.Conditions.GetOrDefault<string>("Contact Information");
+			if (!string.IsNullOrEmpty(contact)) {
+				var contactRepository = Application.Ioc.Resolve<IRepository<Domain.Entities.UserContact, Guid>>();
+				var contactQuery = contactRepository.Query();
+				query = query.Join(contactQuery, u => u.Id, c => c.User.Id, (u, c) => new { u, c })
+					.Where(p =>
+						p.c.Email.Contains(contact) ||
+						p.c.Mobile.Contains(contact) ||
+						p.c.Tel.Contains(contact) ||
+						p.c.Address.Contains(contact) ||
+						p.c.QQ.Contains(contact))
+					.Select(p => p.u);
+			}
+		}
 
 		public void OnSort(AjaxTableSearchRequest request, ref IQueryable<User> query) { }
 
